85102972c27adf7ee1f896eb9a5706510b067660
unknown <ginwoon.soong@PJ8L-DFFL38W1.gfk.com>
2015-08-26 16:02:06 +0800

    done without UI implement

--- src/script.js
+++ src/script.js
@@ -1,1 +1,46 @@
-angular.module('app.memberList', []);
+(function(){
+var app = angular.module('app.memberList', []);
+
+app.controller('KlDevMembers', function(){
+	 this.profiles = bio;
+ });
+
+var bio = [
+	 {
+		 Id: '1',
+		 Name: 'Gin',
+		 Email: 'gin@mail.com',
+		 Phone: '012-2911560',
+		 Date : '25-8-2015'
+	 }
+];
+var IdNumber = 1;	 
+
+
+app.controller("MemberController", function(){
+	this.profile = {};
+	
+	this.addMember = function(){
+		var created = new Date();
+		IdNumber++;
+		var data = {
+		Id : IdNumber,
+		Name : this.profile.Name,
+		Email : this.profile.Email,
+		Phone: this.profile.Phone,
+		Date: created.getDate() + "-" + (created.getMonth() + 1) + "-" + created.getFullYear()
+		};
+		bio.push(data);
+		this.profile = {};
+	};
+	
+	this.deleteMember = function(delMember){
+		 bio.splice(delMember,1);	
+	}
+	
+});
+
+
+
+
+})();
--- src/test/script.spec.js
+++ src/test/script.spec.js
@@ -1,5 +1,60 @@
-describe('script.js Tests', function() {
+xdescribe('script.js Tests', function() {
     it('should be able to run with \'karma run\'', function() {
         expect(true).toBeTruthy();
     });
+});
+
+xdescribe('KlDevMembersController Test', function() {
+	beforeEach(module('app.memberList'));
+	
+	var $controller;
+	beforeEach(inject(function(_$controller_){
+		$controller = _$controller_;
+	}));
+	
+	describe('read global array', function(){
+		it('read the global array to store member list', function(){
+			var controller = $controller('KlDevMembers', {});
+			expect(controller.profiles[0].Name).toEqual('Gin');
+		});
+	});
+});
+
+describe('MemberController Test', function(){
+	beforeEach(module('app.memberList'));
+	
+	var $controller;
+	beforeEach(inject(function(_$controller_){
+		$controller = _$controller_;
+	}));
+	
+	describe('Adding data into the array Test', function(){
+		it('Append the array', function(){
+			var controllerRead = $controller('KlDevMembers', {});
+			var controllerAdd = $controller('MemberController', {});
+			var data = {
+				Name: 'Test',
+				Email: 'test@mail.com',
+				Phone: '012-3456789',
+				};
+			controllerAdd.profile = data;
+			controllerAdd.addMember();
+			expect(controllerRead.profiles[1].Name).toEqual('Test');
+		});
+	});
+	
+	describe('Remove data from the array Test', function(){
+		it('Delete the array', function(){
+			var controllerRead = $controller('KlDevMembers', {});
+			var controllerDel = $controller('MemberController', {});
+			var data = 
+			{
+				Name: 'Test',
+				Email: 'test@mail.com',
+				Phone: '012-3456789',
+			 }
+			 controllerDel.deleteMember();
+			 expect(controllerRead.profiles.length).toEqual(1);
+		});
+	});
 });
